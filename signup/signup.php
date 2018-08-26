b0b2c141f7cb9c0be8d876759ac0200c311ad86b
Arjun Aravind <arjun.aravind1998@gmail.com>
2018-08-26 12:33:34 -0700

    Edited the signup.php file to connect to arjun's godaddy server for testing purposes. Also, created some error messages.

--- signup/signup.php
+++ signup/signup.php
@@ -3,7 +3,7 @@
 $dbservername="localhost";
 $dbusername= "arjun1098";
 $dbpassword="Superman123!";
-$dbname="register";
+$dbname="INTERRUPT";
 
 $connect=mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
 if (!$connect) {
@@ -37,45 +37,47 @@ if (!$connect) {
 
 
 //to validate email and phone no
-	if($checkEmail && $checkMobile)
-	{
-	$email=$_POST['emailInput'];
-	$mobile=$_POST['numberInput'];
+	if($checkEmail && $checkMobile){
+		$email=$_POST['emailInput'];
+		$mobile=$_POST['numberInput'];
 
-	//sql cmd fr querying 
+		//sql cmd fr querying 
 
-	$sql="INSERT INTO users (mobileNo,password,college,name,email) VALUES ('$mobile', '$password','$college','$name','$email'); INSERT INTO events (mobileNo,GoodWillHunting,TheGameOfCodes,Predestination,TheDigitalFortress,TheSecretSociety,UnicornOfSilicon,FishBowlConversation,Inquizitive,MiniProject,PresentationFrankenstein) VALUES ('$mobile','$GoodWillHunting','$TheGameOfCodes','$Predestination','$TheDigitalFortress','$TheSecretSociety','$UnicornOfSilicon','$FishBowlConversation','$Inquizitive','$MiniProject','$PresentationFrankenstein');";
+		$sql="INSERT INTO users (mobileNo,password,college,name,email) VALUES ('$mobile', '$password','$college','$name','$email'); INSERT INTO events (mobileNo,GoodWillHunting,TheGameOfCodes,Predestination,TheDigitalFortress,TheSecretSociety,UnicornOfSilicon,FishBowlConversation,Inquizitive,MiniProject,PresentationFrankenstein) VALUES ('$mobile','$GoodWillHunting','$TheGameOfCodes','$Predestination','$TheDigitalFortress','$TheSecretSociety','$UnicornOfSilicon','$FishBowlConversation','$Inquizitive','$MiniProject','$PresentationFrankenstein');";
 
- 	//sending those queries to db
+	 	//sending those queries to db
 
-	if (mysqli_multi_query($connect, $sql)) {
-          echo "success"; }
+		if(mysqli_multi_query($connect, $sql)) {
+	          echo "<script>alert('You signed up successfully! You can edit your selected events by using the 'login' option. Thanks!');</script>";
+			  echo "<script>window.location.href='http://www.arjunaravind.in/interrupt/signup/';</script>";
+		}
 
     }
 
     else{
-    	header('Location: index.html?signup=invalid');
-
+    	echo "<script>alert('Oops! You seem to have made a mistake! Try again!');</script>";
+		echo "<script>window.location.href='http://www.arjunaravind.in/interrupt/signup/';</script>";
     }
 
 
 //a function to validate email address
 
 function validEmail($email){
-// events if the formatting is correct
-if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-return FALSE;
-}
-//events whether the domain is real or not.
-$domain = explode("@", $email, 2);
-return checkdnsrr($domain[1]); // returns TRUE/FALSE;
+	// events if the formatting is correct
+	if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+	return FALSE;
+	}
+	//events whether the domain is real or not.
+	$domain = explode("@", $email, 2);
+	return checkdnsrr($domain[1]); // returns TRUE/FALSE;
 }
 
 
 //events if there are ten digits in a phone no
-function validMobile($mobile)
-{
+function validMobile($mobile){
     return preg_match('/^[0-9]{10}+$/', $mobile);
 }
+
 mysqli_close($connect);
+
 ?>
