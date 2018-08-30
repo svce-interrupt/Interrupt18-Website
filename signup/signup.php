e2e7c60bcbff800beabbc45fef7be98ce3742b9a
SHRIRAM0509 <41752215+SHRIRAM0509@users.noreply.github.com>
2018-08-30 10:11:14 +0530

    Update signup.php
    a mail will be sent to the user once he registers

--- signup/signup.php
+++ signup/signup.php
@@ -52,7 +52,7 @@ if (!$connect) {
 
 		if(mysqli_multi_query($connect, $sql)) {
 	          echo "<script>alert('You signed up successfully! You can edit your selected events by using the 'login' option. Thanks!');</script>";
-	          mail($email,$subject,$message,$headers);
+	          mail($email,$subject,$message,$headers); //can only be checked in a proper server,can't be checked in local server :-(
 			  echo "<script>window.location.href='http://www.arjunaravind.in/interrupt/signup/';</script>";
 		}
 
