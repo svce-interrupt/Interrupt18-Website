5e6f90f8734c8790901d2a12bfa04772549400af
arjun1098 <arjun.aravind1998@gmail.com>
2018-09-08 15:49:03 +0530

    Added Apple Favicon.

--- login/index.php
+++ login/index.php
@@ -7,6 +7,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet">
 		<link href="css/index-css.css" rel="stylesheet">
 		<link rel="icon" href="../img/favicon.jpeg">
+		<link rel="apple-touch-icon" href="img/favicon.jpeg">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<?php 
