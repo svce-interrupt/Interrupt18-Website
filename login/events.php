2e3ad4ca778f196ed57783e471c1a983cae357f9
arjun1098 <arjun.aravind1998@gmail.com>
2018-09-08 15:45:29 +0530

    Added viewport method.

--- login/events.php
+++ login/events.php
@@ -7,6 +7,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet"> 
 		<link rel="stylesheet" href="css/events-css.css">
 		<link rel="icon" href="../img/favicon.jpeg">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	
 	<?php 
