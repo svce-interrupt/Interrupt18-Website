9824d5483ca158aad70baccb028cecaef4249162
arjun1098 <arjun.aravind1998@gmail.com>
2018-09-08 14:18:08 +0530

    Copied eventsMobile.php

--- login/eventsMobile.php
+++ login/eventsMobile.php
@@ -2,6 +2,7 @@
 <html>
 	
 	<head>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 		<title>Interrupt '18 | Registered Events</title>
 		<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet"> 
 		<link rel="stylesheet" href="css/events-css.css">
@@ -28,21 +29,20 @@
 			$LogiciansCode=$_POST['event1'];
 			$PitchPerfect=$_POST['event2'];
 			$Inquizitive=$_POST['event3'];
-			$FlexboxFroggy=$_POST['event4'];
-			$FinishTheCode=$_POST['event5'];
-			$TerminalOfSuccess=$_POST['event6'];
+			$ArtAttack=$_POST['event4'];
+			$ClashOfCodes=$_POST['event5'];
+			$TerminalOfSecrets=$_POST['event6'];
 			$PresentationHub=$_POST['event7'];
-			$TechCrunch=$_POST['event8'];
-			$TechnoFair=$_POST['event9'];
-			$InterruptTheChallenge=$_POST['event10'];
-			$PipeThePiper=$_POST['event11'];
-			$DataScience=$_POST['event12'];
-			$WorkshopAWS=$_POST['event13'];
+			$TechnoFair=$_POST['event8'];
+			$InterruptChallenge=$_POST['event9'];
+			$PipeThePiper=$_POST['event10'];
+			$Dataification=$_POST['event11'];
+			$WorkshopAWS=$_POST['event12'];
 
 			//sql cmd fr updating change of events
 	    	//updates only the row with correct mobileNo
 
-			$sql="UPDATE events set LogiciansCode='$LogiciansCode',PitchPerfect='$PitchPerfect',Inquizitive='$Inquizitive',FlexboxFroggy='$FlexboxFroggy',FinishTheCode='$FinishTheCode',TerminalOfSuccess='$TerminalOfSuccess',PresentationHub='$PresentationHub',TechCrunch='$TechCrunch',TechnoFair='$TechnoFair',InterruptTheChallenge='$InterruptTheChallenge',PipeThePiper='$PipeThePiper',DataScience='$DataScience',WorkshopAWS='$WorkshopAWS' WHERE mobileNo='$mobile';";
+			$sql="UPDATE events set LogiciansCode=$LogiciansCode,PitchPerfect=$PitchPerfect,Inquizitive=$Inquizitive,ArtAttack=$ArtAttack,ClashOfCodes=$ClashOfCodes,TerminalOfSecrets=$TerminalOfSecrets,PresentationHub=$PresentationHub,TechnoFair=$TechnoFair,InterruptChallenge=$InterruptChallenge,PipeThePiper=$PipeThePiper,Dataification=$Dataification,WorkshopAWS=$WorkshopAWS WHERE mobileNo='$mobile';";
 
 			
 			//sending those queries to db and if query successful, redirect to login/index.php
@@ -60,8 +60,7 @@
 		if(isset($_SESSION['mobile'])) {
 			header('Location: ');
 		} else {
-			$_SESSION['message'] = "Error: Please login";
-			echo "<script>window.location.href='index.php'</script>";
+			header('Location: ../login');
 		}
 	?>
 	
@@ -84,45 +83,42 @@
 			<p class='form-text'>The highlighted ones are events you've already registered for! If you'd like to register for more events click on them, and click on the highlighted events again to register from the respected event! <span style='width:5%;' </span></p>
 
 			<input type="hidden" id="input1" name='event1' value="0">
-			<span class='home-links check blue top' id='check1'>Logician's Code</span>
+			<span class='home-links check orange top' id='check1'>Logician's Code</span>
 			
 			<input type="hidden" id="input2" name='event2' value="0">
 			<span class='home-links check orange top' id='check2'>Pitch Perfect</span>
 			
 			<input type="hidden" id="input3" name="event3" value="0">
-			<span class='home-links check red1 top' id='check3'>Inquizitive</span>
+			<span class='home-links check orange top' id='check3'>Inquizitive</span>
 			
 			<input type="hidden" id="input4" name="event4" value="0">
-			<span class='home-links check red1' id='check4'>Flexbox Froggy</span>
+			<span class='home-links check lilac' id='check4'>Art Attack</span>
 			
 			<input type="hidden" id="input5" name="event5" value="0">
-			<span class='home-links check blue' id='check5'>Finish The Code</span>
+			<span class='home-links check lilac' id='check5'>Clash Of Codes</span>
 			
 			<input type="hidden" id="input6" name="event6" value="0">
-			<span class='home-links check orange' id='check6'>Terminal Of Secrets</span>
+			<span class='home-links check lilac' id='check6'>Terminal Of Secrets</span>
 			
 			<input type="hidden" id="input7" name="event7" value="0">
-			<span class='home-links check orange' id='check7'>Presentation Hub</span>
+			<span class='home-links check light-red' id='check7'>Presentation Hub</span>
 			
 			<input type="hidden" id="input8" name="event8" value="0">
-			<span class='home-links check red1' id='check8'>TechCrunch</span>
+			<span class='home-links check light-red' id='check8'>TechnoFair</span>
 			
 			<input type="hidden" id="input9" name="event9" value="0">
-			<span class='home-links check blue' id='check9'>TechnoFair</span>
+			<span class='home-links check light-red' id='check9'>Interrupt Challenge</span>
 			
 			<input type="hidden" id="input10" name="event10" value="0">
-			<span class='home-links check red1' id='check10'>Interrupt The Challenge</span>
+			<span class='home-links check cyan' id='check10'>Pipe The Piper</span>
 			
 			<input type="hidden" id="input11" name="event11" value="0">
-			<span class="home-links check blue" id="check11">Pipe The Piper</span>
+			<span class="home-links check cyan" id="check11">Dataification</span>
 
 			<input type="hidden" id="input12" name="event12" value="0">
-			<span class="home-links check orange" id="check12">Data Science</span>			
+			<span class="home-links check cyan" id="check12">Workshop AWS</span>
 
-			<input type="hidden" id="input13" name="event13" value="0">
-			<span class="home-links check red1" id="check13">Workshop AWS</span>
-
-			<input type='submit' name='submit' value="Save" class='home-links save'>
+			<input type='submit' name='submit' value="Save" class='home-links submit-butt'>
 
 			
 		</form>
@@ -150,112 +146,89 @@
 			var check10=<?php echo $_SESSION['ev'][9] ?>;
 			var check11=<?php echo $_SESSION['ev'][10] ?>;
 			var check12=<?php echo $_SESSION['ev'][11] ?>;
-			var check13=<?php echo $_SESSION['ev'][12] ?>;
 
 			// if event is already selected in db, highlight event
 			if (check1 == 1) {
-				document.getElementById("check1").style.border="1px dashed yellow";
-				document.getElementById("check1").style.color="yellow";
+				document.getElementById("check1").classList.add("yellow");
 				document.getElementById("input1").value = '1';
 			}
 
 			if (check2 == 1) {
-				document.getElementById("check2").style.border="1px dashed yellow";
-				document.getElementById("check2").style.color="yellow";
+				document.getElementById("check2").classList.add("yellow");
 				document.getElementById("input2").value = '1';
 			}
 
 			if (check3 == 1) {
-				document.getElementById("check3").style.border="1px dashed yellow";
-				document.getElementById("check3").style.color="yellow";
+				document.getElementById("check3").classList.add("yellow");
 				document.getElementById("input3").value = '1';
 			}
 
 			if (check4 == 1) {
-				document.getElementById("check4").style.border="1px dashed yellow";
-				document.getElementById("check4").style.color="yellow";
+				document.getElementById("check4").classList.add("yellow");
 				document.getElementById("input4").value = '1';
 			}
 
 			if (check5 == 1) {
-				document.getElementById("check5").style.border="1px dashed yellow";
-				document.getElementById("check5").style.color="yellow";
+				document.getElementById("check5").classList.add("yellow");
 				document.getElementById("input5").value = '1';
 			}
 
 			if (check6 == 1) {
-				document.getElementById("check6").style.border="1px dashed yellow";
-				document.getElementById("check6").style.color="yellow";
+				document.getElementById("check6").classList.add("yellow");
 				document.getElementById("input6").value = '1';
 			}
 
 			if (check7 == 1) {
-				document.getElementById("check7").style.border="1px dashed yellow";
-				document.getElementById("check7").style.color="yellow";
+				document.getElementById("check7").classList.add("yellow");
 				document.getElementById("input7").value = '1';
 			}
 
 			if (check8 == 1) {
-				document.getElementById("check8").style.border="1px dashed yellow";
-				document.getElementById("check8").style.color="yellow";
+				document.getElementById("check8").classList.add("yellow");
 				document.getElementById("input8").value = '1';
 			}
 
 			if (check9 == 1) {
-				document.getElementById("check9").style.border="1px dashed yellow";
-				document.getElementById("check9").style.color="yellow";
+				document.getElementById("check9").classList.add("yellow");
 				document.getElementById("input9").value = '1';
 			}
 
 			if (check10 == 1) {
-				document.getElementById("check10").style.border="1px dashed yellow";
-				document.getElementById("check10").style.color="yellow";
+				document.getElementById("check10").classList.add("yellow");
 				document.getElementById("input10").value = '1';
 			}
 
 			if (check11 == 1) {
-				document.getElementById("check11").style.border="1px dashed yellow";
-				document.getElementById("check11").style.color="yellow";
+				document.getElementById("check11").classList.add("yellow");
 				document.getElementById("input11").value = '1';
 			}
 			if (check12 == 1) {
-				document.getElementById("check12").style.border="1px dashed yellow";
-				document.getElementById("check12").style.color="yellow";
+				document.getElementById("check12").classList.add("yellow");
 				document.getElementById("input12").value = '1';
-			}
-			if (check13 == 1) {
-				document.getElementById("check13").style.border="1px dashed yellow";
-				document.getElementById("check13").style.color="yellow";
-				document.getElementById("input13").value = '1';
 			}
 
 			document.getElementById("check1").addEventListener("click", function(){
+
 				if(check1==0){
-					
-					document.getElementById("check1").style.border="1px dashed yellow";
-					document.getElementById("check1").style.color="yellow";
+					document.getElementById("check1").classList.add("yellow");
 					document.getElementById("input1").value = '1';
 					check1=1;
 				}
 				else{
-					
-					document.getElementById("check1").style.border="1px dashed #29b6f6";
+					document.getElementById("check1").classList.remove("yellow");
 					document.getElementById("input1").value = '0';
-					document.getElementById("check1").style.color="#29b6f6";
 					check1=0;
 				}
 			});
 		
 			document.getElementById("check2").addEventListener("click", function(){
 				if(check2==0){
-					document.getElementById("check2").style.border="1px dashed yellow";
-					document.getElementById("check2").style.color="yellow";
+					document.getElementById("check2").classList.add("yellow");
 					document.getElementById("input2").value = '1';
 					check2=1;
 				}
 				else{
-					document.getElementById("check2").style.border="1px dashed #FB8C00";
-					document.getElementById("check2").style.color="#FB8C00";
+					document.getElementById("check2").classList.remove("yellow");
 					document.getElementById("input2").value = '0';
 					check2=0;
 				}
@@ -263,14 +236,12 @@
 		
 			document.getElementById("check3").addEventListener("click", function(){
 				if(check3==0){
-					document.getElementById("check3").style.border="1px dashed yellow";
-					document.getElementById("check3").style.color="yellow";
+					document.getElementById("check3").classList.add("yellow");
 					document.getElementById("input3").value = '1';
 					check3=1;
 				}
 				else{
-					document.getElementById("check3").style.border="1px dashed #EF9A9A";
-					document.getElementById("check3").style.color="#EF9A9A";
+					document.getElementById("check3").classList.remove("yellow");
 					document.getElementById("input3").value = '0';
 					check3=0;
 				}
@@ -278,14 +249,12 @@
 	
 			document.getElementById("check4").addEventListener("click", function(){
 				if(check4==0){
-					document.getElementById("check4").style.border="1px dashed yellow";
-					document.getElementById("check4").style.color="yellow";
+					document.getElementById("check4").classList.add("yellow");
 					document.getElementById("input4").value = '1';
 					check4=1;
 				}
 				else{
-					document.getElementById("check4").style.border="1px dashed #EF9A9A";
-					document.getElementById("check4").style.color="#EF9A9A";
+					document.getElementById("check4").classList.remove("yellow");
 					document.getElementById("input4").value = '0';
 					check4=0;
 				}
@@ -293,14 +262,12 @@
 		
 			document.getElementById("check5").addEventListener("click", function(){
 				if(check5==0){
-					document.getElementById("check5").style.border="1px dashed yellow";
-					document.getElementById("check5").style.color="yellow";
+					document.getElementById("check5").classList.add("yellow");
 					document.getElementById("input5").value = '1';
 					check5=1;
 				}
 				else{
-					document.getElementById("check5").style.border="1px dashed #29b6f6";
-					document.getElementById("check5").style.color="#29b6f6";
+					document.getElementById("check5").classList.remove("yellow");
 					document.getElementById("input5").value = '0';
 					check5=0;
 			
@@ -309,14 +276,12 @@
 							
 			document.getElementById("check6").addEventListener("click", function(){
 				if(check6==0){
-					document.getElementById("check6").style.border="1px dashed yellow";
-					document.getElementById("check6").style.color="yellow";
+					document.getElementById("check6").classList.add("yellow");
 					document.getElementById("input6").value = '1';
 					check6=1;
 				}
 				else{
-					document.getElementById("check6").style.border="1px dashed #FB8C00";
-					document.getElementById("check6").style.color="#FB8C00";
+					document.getElementById("check6").classList.remove("yellow");
 					document.getElementById("input6").value = '0';
 					check6=0;
 				}
@@ -324,14 +289,12 @@
 		
 			document.getElementById("check7").addEventListener("click", function(){
 				if(check7==0){
-					document.getElementById("check7").style.border="1px dashed yellow";
-					document.getElementById("check7").style.color="yellow";
+					document.getElementById("check7").classList.add("yellow");
 					document.getElementById("input7").value = '1';
 					check7=1;
 				}
 				else{
-					document.getElementById("check7").style.border="1px dashed #FB8C00";
-					document.getElementById("check7").style.color="#FB8C00";
+					document.getElementById("check7").classList.remove("yellow");
 					document.getElementById("input7").value = '0';
 					check7=0;
 				}
@@ -339,14 +302,12 @@
 		
 			document.getElementById("check8").addEventListener("click", function(){
 				if(check8==0){
-					document.getElementById("check8").style.border="1px dashed yellow";
-					document.getElementById("check8").style.color="yellow";
+					document.getElementById("check8").classList.add("yellow");
 					document.getElementById("input8").value = '1';
 					check8=1;
 				}
 				else{
-					document.getElementById("check8").style.border="1px dashed #EF9A9A";
-					document.getElementById("check8").style.color="#EF9A9A";
+					document.getElementById("check8").classList.remove("yellow");
 					document.getElementById("input8").value = '0';
 					check8=0;
 				}
@@ -354,14 +315,12 @@
 		
 			document.getElementById("check9").addEventListener("click", function(){
 				if(check9==0){
-					document.getElementById("check9").style.border="1px dashed yellow";
-					document.getElementById("check9").style.color="yellow";
+					document.getElementById("check9").classList.add("yellow");
 					document.getElementById("input9").value = '1';
 					check9=1;
 				}
 				else{
-					document.getElementById("check9").style.border="1px dashed #29b6f6";
-					document.getElementById("check9").style.color="#29b6f6";
+					document.getElementById("check9").classList.remove("yellow");
 					document.getElementById("input9").value = '0';
 					check9=0;
 				}
@@ -369,58 +328,38 @@
 		
 			document.getElementById("check10").addEventListener("click", function(){
 				if(check10==0){
-					document.getElementById("check10").style.border="1px dashed yellow";
-					document.getElementById("check10").style.color="yellow";
+					document.getElementById("check10").classList.add("yellow");
 					document.getElementById("input10").value = '1';
 					check10=1;
 				}
 				else{
-					document.getElementById("check10").style.border="1px dashed #EF9A9A";
-					document.getElementById("check10").style.color="#EF9A9A";
+					document.getElementById("check10").classList.remove("yellow");
 					document.getElementById("input10").value = '0';
 					check10=0;
 				}
 			});
 			document.getElementById("check11").addEventListener("click", function(){
 				if(check11==0){
-					document.getElementById("check11").style.border="1px dashed yellow";
-					document.getElementById("check11").style.color="yellow";
+					document.getElementById("check11").classList.add("yellow");
 					document.getElementById("input11").value = '1';
 					check11=1;
 				}
 				else{
-					document.getElementById("check11").style.border="1px dashed #EF9A9A";
-					document.getElementById("check11").style.color="#EF9A9A";
+					document.getElementById("check11").classList.remove("yellow");
 					document.getElementById("input11").value = '0';
 					check11=0;
 				}
 			});
 			document.getElementById("check12").addEventListener("click", function(){
 				if(check12==0){
-					document.getElementById("check12").style.border="1px dashed yellow";
-					document.getElementById("check12").style.color="yellow";
+					document.getElementById("check12").classList.add("yellow");
 					document.getElementById("input12").value = '1';
 					check12=1;
 				}
 				else{
-					document.getElementById("check12").style.border="1px dashed #EF9A9A";
-					document.getElementById("check12").style.color="#EF9A9A";
+					document.getElementById("check12").classList.remove("yellow");
 					document.getElementById("input12").value = '0';
 					check12=0;
-				}
-			});
-			document.getElementById("check13").addEventListener("click", function(){
-				if(check13==0){
-					document.getElementById("check13").style.border="1px dashed yellow";
-					document.getElementById("check13").style.color="yellow";
-					document.getElementById("input13").value = '1';
-					check13=1;
-				}
-				else{
-					document.getElementById("check13").style.border="1px dashed #EF9A9A";
-					document.getElementById("check13").style.color="#EF9A9A";
-					document.getElementById("input13").value = '0';
-					check13=0;
 				}
 			});
 	</script>
