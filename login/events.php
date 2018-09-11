8daffdd31351a86caca6220edfaca13c0919e181
arjun1098 <arjun.aravind1998@gmail.com>
2018-09-11 21:21:43 +0530

    Add metadata to all visible HTML and PHP files in the website.

--- login/events.php
+++ login/events.php
@@ -2,12 +2,16 @@
 <html>
 	
 	<head>
-		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 		<title>Interrupt '18 | Registered Events</title>
 		<link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet"> 
 		<link rel="stylesheet" href="css/events-css.css">
 		<link rel="icon" href="../img/favicon.jpeg">
 		<link rel="apple-touch-icon" href="img/favicon.jpeg">
+
+		<meta charset="UTF-8">
+                <meta name="description" content="Interrupt 2018 Official Website">
+                <meta name="keywords" content="interrupt,2018,2k18,svce,symposium,event,tech">
+                <meta name="author" content="SVCE Interrupt Web Team 2018">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	
